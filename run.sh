42c5747841e5c5d2f4e08c3888db2cfee2de95c5
yalpha <clement.naissans@gmail.com>
2026-01-31 22:20:24 +0100

    changement run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,7 @@ docker run -d --name db --network my-tiny-network \
   -e POSTGRES_USER=admin \
   -e POSTGRES_PASSWORD=secret \
   -e POSTGRES_DB=workshop \
-  -e PGDATA=/data \
-  -v db-vol:/data \
+  -v db-vol:/var/lib/postgresql/data \
   -v $(pwd)/init.sql:/docker-entrypoint-initdb.d/init.sql \
   db
 
