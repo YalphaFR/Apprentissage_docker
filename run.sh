1c4ed8d6fe543ab9acbc0498bf1ef89ee569b276
yalpha <clement.naissans@gmail.com>
2026-03-03 15:32:41 +0100

    ajout 3 items par défaut

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ docker run -d --name db --network my-tiny-network \
   -e POSTGRES_USER=admin \
   -e POSTGRES_PASSWORD=secret \
   -e POSTGRES_DB=workshop \
-  -e PGDATA=/var/lib/postgresql/data \
-  -v db-vol:/var/lib/postgresql/data \
+  -e PGDATA=/data \
+  -v db-vol:/data \
   -v $(pwd)/init.sql:/docker-entrypoint-initdb.d/init.sql \
   db
 
